64bf01bb7a0cc138c790a7beb275bc71522eeb01
corruptzero <prodbycorrupt@gmail.com>
2022-12-29 15:57:15 +0300

    modify withdraw service

--- src/main/java/ru/corruptzero/eleftheriaback/service/WithdrawService.java
+++ src/main/java/ru/corruptzero/eleftheriaback/service/WithdrawService.java
@@ -1,14 +1,72 @@
 package ru.corruptzero.eleftheriaback.service;
 
-import ru.corruptzero.eleftheriaback.domain.entity.Withdraw;
+import ru.corruptzero.eleftheriaback.domain.entity.withdraw.EWithdrawStatus;
+import ru.corruptzero.eleftheriaback.domain.entity.withdraw.Withdraw;
+import ru.corruptzero.eleftheriaback.exception.EmptyEntityListException;
+import ru.corruptzero.eleftheriaback.exception.EntityDeleteException;
+import ru.corruptzero.eleftheriaback.exception.EntityNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service that provides CRUD operations for {@link Withdraw} objects.
+ *
+ * <p>Copyright (c) 2023 corruptzero</p>
+ * <p>Licensed under the MIT License.</p>
+ *
+ * @author corruptzero
+ */
 public interface WithdrawService {
+
+    /**
+     * Saves a given withdraw.
+     *
+     * @param withdraw the withdraw to save
+     * @return the saved withdraw
+     */
     Withdraw save(Withdraw withdraw);
+
+    /**
+     * Returns a list of all withdraws.
+     *
+     * @return the list of all withdraws
+     * @throws EmptyEntityListException if no withdraws are found
+     */
     List<Withdraw> getAllWithdraws();
+
+    /**
+     * Returns the withdraw with the given ID, if it exists.
+     *
+     * @param id the ID of the withdraw to retrieve
+     * @return the withdraw with the given ID, wrapped in an {@link Optional}
+     * @throws EntityNotFoundException if the withdraw is not found
+     */
     Optional<Withdraw> findById(Long id);
+
+    /**
+     * Returns a list of all withdraws by their status.
+     *
+     * @param status the status of the withdraw to retrieve
+     * @return the list of all withdraws with a specified status
+     * @throws EmptyEntityListException if no withdraws are found
+     */
+    List<Withdraw> findAllByStatus(EWithdrawStatus status);
+
+    /**
+     * Deletes the withdraw with the given ID.
+     *
+     * @param id the ID of the withdraw to delete
+     * @throws EntityDeleteException if the withdraw cannot be deleted
+     */
     void deleteById(Long id);
+
+    /**
+     * Deletes all withdraws.
+     * <p>
+     * <strong>Note:</strong> This method is only intended for testing purposes.
+     *
+     * @throws EntityDeleteException if the withdraws cannot be deleted
+     */
     void deleteAll();
 }
--- src/main/java/ru/corruptzero/eleftheriaback/service/impl/WithdrawServiceImpl.java
+++ src/main/java/ru/corruptzero/eleftheriaback/service/impl/WithdrawServiceImpl.java
@@ -1,9 +1,17 @@
 package ru.corruptzero.eleftheriaback.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import ru.corruptzero.eleftheriaback.domain.entity.Withdraw;
+import ru.corruptzero.eleftheriaback.domain.entity.withdraw.EWithdrawStatus;
+import ru.corruptzero.eleftheriaback.domain.entity.withdraw.Withdraw;
 import ru.corruptzero.eleftheriaback.domain.repository.WithdrawRepository;
+import ru.corruptzero.eleftheriaback.dto.WithdrawDTO;
+import ru.corruptzero.eleftheriaback.exception.EmptyEntityListException;
+import ru.corruptzero.eleftheriaback.exception.EntityDeleteException;
+import ru.corruptzero.eleftheriaback.exception.EntityNotFoundException;
+import ru.corruptzero.eleftheriaback.mapper.WithdrawMapper;
 import ru.corruptzero.eleftheriaback.service.WithdrawService;
 
 import java.util.ArrayList;
@@ -11,31 +19,57 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class WithdrawServiceImpl implements WithdrawService {
+class WithdrawServiceImpl implements WithdrawService {
     @Autowired
     private WithdrawRepository withdrawRepository;
 
-    public List<Withdraw> getAllWithdraws(){
-        return new ArrayList<>(withdrawRepository.findAll());
+    private final WithdrawMapper mapper = WithdrawMapper.INSTANCE;
+
+    public List<Withdraw> getAllWithdraws() {
+        List<Withdraw> withdraws = new ArrayList<>(withdrawRepository.findAll());
+        if (withdraws.isEmpty()) {
+            throw new EmptyEntityListException("No withdraws found.");
+        }
+        return withdraws;
     }
 
-    public List<Withdraw> findPendingWithdraws(){
-        return new ArrayList<>(withdrawRepository.findPendingWithdraws());
+    public Optional<Withdraw> findById(Long id) {
+        Optional<Withdraw> withdraw = withdrawRepository.findById(id);
+        if (withdraw.isEmpty()) {
+            throw new EntityNotFoundException("Withdraw not found with ID: " + id);
+        }
+        return withdraw;
     }
 
-    public Optional<Withdraw> findById(Long id){
-        return withdrawRepository.findById(id);
+    @Override
+    public List<Withdraw> findAllByStatus(EWithdrawStatus status) {
+        List<Withdraw> withdraws = new ArrayList<>(withdrawRepository.findAllByStatus(status));
+        if (withdraws.isEmpty()) {
+            throw new EmptyEntityListException("No pending withdraws found.");
+        }
+        return withdraws;
     }
 
-    public Withdraw save(Withdraw withdraw){
+    public Withdraw save(Withdraw withdraw) {
         return withdrawRepository.save(withdraw);
     }
 
+
     public void deleteById(Long id) {
-        withdrawRepository.deleteById(id);
+        try {
+            withdrawRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityDeleteException("Failed to delete withdraw with ID: " + id + ". Entity not found.");
+        } catch (DataAccessException e) {
+            throw new EntityDeleteException("Failed to delete withdraw with ID: " + id + ". Data access error occurred.");
+        }
     }
 
     public void deleteAll() {
-        withdrawRepository.deleteAll();
+        try {
+            withdrawRepository.deleteAll();
+        } catch (DataAccessException e) {
+            throw new EntityDeleteException("Failed to delete all withdraws. Data access error occurred.");
+        }
     }
 }
