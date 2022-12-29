7cb5f82bf4304fc61b5cfff56fb94bdcf33c0d28
corruptzero <prodbycorrupt@gmail.com>
2022-12-29 15:46:18 +0300

    modify OrderService

--- src/main/java/ru/corruptzero/eleftheriaback/service/OrderService.java
+++ src/main/java/ru/corruptzero/eleftheriaback/service/OrderService.java
@@ -1,14 +1,65 @@
 package ru.corruptzero.eleftheriaback.service;
 
-import ru.corruptzero.eleftheriaback.domain.entity.Order;
+import ru.corruptzero.eleftheriaback.domain.entity.order.Order;
+import ru.corruptzero.eleftheriaback.exception.EmptyEntityListException;
+import ru.corruptzero.eleftheriaback.exception.EntityDeleteException;
+import ru.corruptzero.eleftheriaback.exception.EntityNotFoundException;
+import ru.corruptzero.eleftheriaback.exception.InvalidEntityException;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service that provides CRUD operations for {@link Order} objects.
+ *
+ * <p>Copyright (c) 2023 corruptzero</p>
+ * <p>Licensed under the MIT License.</p>
+ *
+ * @author corruptzero
+ */
 public interface OrderService {
+
+    /**
+     * Saves a given order.
+     *
+     * @param order the order to save
+     * @return the saved order
+     * @throws InvalidEntityException if the order is invalid or has the same title as another order
+     */
     Order save(Order order);
+
+    /**
+     * Returns a list of all orders.
+     *
+     * @return the list of all orders
+     * @throws EmptyEntityListException if no orders are found
+     */
     List<Order> getAllOrders();
+
+    /**
+     * Returns the order with the given ID, if it exists.
+     *
+     * @param id the ID of the order to retrieve
+     * @return the order with the given ID, wrapped in an {@link Optional}
+     * @throws EntityNotFoundException if the order is not found
+     */
     Optional<Order> findById(Long id);
+
+    /**
+     * Deletes the order with the given ID.
+     *
+     * @param id the ID of the order to delete
+     * @throws EntityDeleteException if the order cannot be deleted
+     */
     void deleteById(Long id);
+
+    /**
+     * Deletes all orders.
+     * <p>
+     * <strong>Note:</strong> This method is only intended for testing purposes.
+     *
+     * @throws EntityDeleteException if the orders cannot be deleted
+     */
     void deleteAll();
 }
+
--- src/main/java/ru/corruptzero/eleftheriaback/service/impl/OrderServiceImpl.java
+++ src/main/java/ru/corruptzero/eleftheriaback/service/impl/OrderServiceImpl.java
@@ -1,9 +1,15 @@
 package ru.corruptzero.eleftheriaback.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import ru.corruptzero.eleftheriaback.domain.entity.Order;
+import ru.corruptzero.eleftheriaback.domain.entity.order.Order;
 import ru.corruptzero.eleftheriaback.domain.repository.OrderRepository;
+import ru.corruptzero.eleftheriaback.exception.EmptyEntityListException;
+import ru.corruptzero.eleftheriaback.exception.EntityDeleteException;
+import ru.corruptzero.eleftheriaback.exception.EntityNotFoundException;
+import ru.corruptzero.eleftheriaback.exception.InvalidEntityException;
 import ru.corruptzero.eleftheriaback.service.OrderService;
 
 import java.util.ArrayList;
@@ -11,27 +17,51 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class OrderServiceImpl implements OrderService {
+class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-    public List<Order> getAllOrders(){
-        return new ArrayList<>(orderRepository.findAll());
+    public List<Order> getAllOrders() {
+        List<Order> orders = new ArrayList<>(orderRepository.findAll());
+        if (orders.isEmpty()) {
+            throw new EmptyEntityListException("No orders found.");
+        }
+        return orders;
     }
 
-    public Optional<Order> findById(Long id){
-        return orderRepository.findById(id);
+    public Optional<Order> findById(Long id) {
+        Optional<Order> order = orderRepository.findById(id);
+        if (order.isEmpty()) {
+            throw new EntityNotFoundException("Order not found with ID: " + id);
+        }
+        return order;
     }
 
-    public Order save(Order order){
+    public Order save(Order order) {
+        if (orderRepository.existsByTitle(order.getTitle())) {
+            throw new InvalidEntityException("An order with the same title already exists.");
+        }
         return orderRepository.save(order);
     }
 
     public void deleteById(Long id) {
-        orderRepository.deleteById(id);
+        try {
+            orderRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityDeleteException("Failed to delete order with ID: " + id + ". Entity not found.");
+        } catch (DataAccessException e) {
+            throw new EntityDeleteException("Failed to delete order with ID: " + id + ". Data access error occurred.");
+        }
     }
 
     public void deleteAll() {
-        orderRepository.deleteAll();
+        try {
+            orderRepository.deleteAll();
+        } catch (DataAccessException e) {
+            throw new EntityDeleteException("Failed to delete all orders. Data access error occurred.");
+        }
     }
 }
+
+
+
